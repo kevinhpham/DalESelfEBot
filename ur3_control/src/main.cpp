56f8e2e0060dc69403804f7e6236eee18957cbfd
Jarred Deluca <jarred.g.deluca@student.uts.edu.au>
2025-03-18 10:49:48 +1100

    Still testing.

--- ur3_control/src/main.cpp
+++ ur3_control/src/main.cpp
@@ -1,14 +1,29 @@
+// #include "spline_follower.hpp"
+// #include <rclcpp/rclcpp.hpp>
+
+// int main(int argc, char** argv) {
+//     rclcpp::init(argc, argv);
+//     auto node = std::make_shared<SplineFollower>();
+//     // rclcpp::executors::SingleThreadedExecutor executor;
+//     // executor.add_node(node);
+//     // executor.spin();
+//     node->on_activate();
+//     rclcpp::spin(node);
+//     rclcpp::shutdown();
+//     return 0;
+// }
+
 #include "spline_follower.hpp"
 #include <rclcpp/rclcpp.hpp>
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
+
     auto node = std::make_shared<SplineFollower>();
-    // rclcpp::executors::SingleThreadedExecutor executor;
-    // executor.add_node(node);
-    // executor.spin();
-    node->on_activate();
-    rclcpp::spin(node);
+    node->on_activate();  // Starts the state machine
+
+    rclcpp::spin(node);   // Keep spinning while the state machine runs
+
     rclcpp::shutdown();
     return 0;
 }
