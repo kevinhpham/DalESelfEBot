3c9b0533d55ac23578e2eb209516700345a44b1f
Jarred Deluca <jarred.g.deluca@student.uts.edu.au>
2025-03-25 17:08:18 +1100

    Cleaned up ur3_control.

--- ur3_control/src/main.cpp
+++ ur3_control/src/main.cpp
@@ -1,76 +1,9 @@
-// #include "spline_follower.hpp"
-// #include <rclcpp/rclcpp.hpp>
-
-// int main(int argc, char** argv) {
-//     rclcpp::init(argc, argv);
-//     auto node = std::make_shared<SplineFollower>();
-//     // rclcpp::executors::SingleThreadedExecutor executor;
-//     // executor.add_node(node);
-//     // executor.spin();
-//     node->on_activate();
-//     rclcpp::spin(node);
-//     rclcpp::shutdown();
-//     return 0;
-// }
-
 #include <memory>
 #include <thread>
 
 #include "spline_follower.hpp"
 #include <rclcpp/rclcpp.hpp>
 
-void load_parameters_from_yaml(std::shared_ptr<rclcpp::Node> node, const std::string &yaml_file)
-{
-    YAML::Node yaml = YAML::LoadFile(yaml_file);
-    for (auto it = yaml.begin(); it != yaml.end(); ++it)
-    {
-        const auto &param_name = it->first.as<std::string>();
-        const auto &param_value = it->second;
-
-        if (param_value.IsScalar())
-        {
-            node->set_parameter(rclcpp::Parameter(param_name, param_value.as<std::string>()));
-        }
-        else if (param_value.IsSequence())
-        {
-            std::vector<double> values;
-            for (const auto &val : param_value)
-            {
-                values.push_back(val.as<double>());
-            }
-            node->set_parameter(rclcpp::Parameter(param_name, values));
-        }
-        // Add more cases if you expect more complex structures
-    }
-}
-
-// int main(int argc, char** argv) {
-//     rclcpp::init(argc, argv);
-
-//     auto node_one = std::make_shared<rclcpp::Node>("hello_moveit");
-
-//     // Point this to your kinematics.yaml
-//     std::string yaml_file = "/home/jarred/git/DalESelfEBot/ur3_control/config/kinematics.yaml";
-//     load_parameters_from_yaml(node_one, yaml_file);
-
-//     auto node = std::make_shared<SplineFollower>();
-//     // node->on_activate();  // Starts the state machine
-
-//     // rclcpp::spin(node);   // Keep spinning while the state machine runs
-
-//     // Spin up a SingleThreadedExecutor for the current state monitor
-//     rclcpp::executors::SingleThreadedExecutor executor;
-//     executor.add_node(node);
-//     executor.add_node(node_one);
-//     auto spinner = std::thread([&executor]() { executor.spin(); });
-
-//     node->on_activate();  // Starts the state machine
-
-//     rclcpp::shutdown();
-
-//     spinner.join();
-//     return 0;
-// }
 
 int main(int argc, char * argv[])
 {
@@ -93,88 +26,27 @@ int main(int argc, char * argv[])
     using moveit::planning_interface::MoveGroupInterface;
     auto move_group_interface = MoveGroupInterface(node, "ur_manipulator");
 
-    // // Get the current end-effector pose
-    // auto const end_effector_pose = move_group_interface.getCurrentPose();
-    // // Log the current end-effector pose
-    // RCLCPP_INFO(logger, "End-effector pose: %f %f %f",
-    //     end_effector_pose.pose.position.x,
-    //     end_effector_pose.pose.position.y,
-    //     end_effector_pose.pose.position.z
-    // );
+    move_group_interface.setMaxVelocityScalingFactor(1.0);  // 1.0 = 100% of max velocity
+    move_group_interface.setMaxAccelerationScalingFactor(1.0); // 1.0 = 100% of max acceleration
+    // move_group_interface.setPlannerId("PTP");
 
     auto control = std::make_shared<SplineFollower>();
-    // control->on_activate();  // Starts the state machine
-
-    // control->setSafeStartPose();
-
-    // // geometry_msgs::msg::Pose new_pose;
-    // // new_pose.position.x = 0.25;
-    // // new_pose.position.y = 0.25;
-    // // new_pose.position.z = 0.25;
-
-    // std::vector<geometry_msgs::msg::Pose> path_to_safe_start = 
-    // control->computeLinearInterpolationPath(end_effector_pose.pose, control->safe_start_pose_, 10);
-
-    // moveit_msgs::msg::RobotTrajectory trajectory;
-    // // double fraction = move_group_interface.computeCartesianPath(path_to_safe_start, 0.00001, 0.0, trajectory);
-
-    // move_group_interface.setMaxVelocityScalingFactor(1.0);
-    // move_group_interface.setGoalTolerance(0.001);
-
-    // const double jump_threshold = 0.0;
-    // const double eef_step = 1.0;
-    // double fraction = move_group_interface.computeCartesianPath(path_to_safe_start, eef_step, jump_threshold, trajectory);
-    // moveit::planning_interface::MoveGroupInterface::Plan my_plan2;
-    // my_plan2.trajectory_ = trajectory;
-    // move_group_interface.execute(my_plan2);
-
-    // if (fraction < 0.95) { 
-    //     RCLCPP_ERROR(logger, "Only achieved %.2f%% of the path", fraction * 100);
-    // }
-
-    // moveit::planning_interface::MoveGroupInterface::Plan plan;
-    // plan.trajectory_ = trajectory;
-    // move_group_interface.execute(plan);
 
     control->loadSplines();
+
+    control->addGroundPlane();
 
     while (rclcpp::ok()) {
         switch (control->state_) {
             case SplineFollower::State::INIT:
             {
-                // auto const end_effector_pose = move_group_interface.getCurrentPose();
+                RCLCPP_INFO(logger, "State: Init");
                 // Set the safe start pose based on localization data
                 control->setSafeStartPose();
 
                 move_group_interface.setPoseTarget(control->safe_start_pose_);
                 move_group_interface.move();
-
-                // const std::vector<geometry_msgs::msg::Pose> path_to_safe_start = 
-                // control->computeLinearInterpolationPath(end_effector_pose.pose, control->safe_start_pose_, 10);
-
-                
-
-                // move_group_interface.setMaxVelocityScalingFactor(1.0);
-                // move_group_interface.setGoalTolerance(0.005);
-
-                // moveit_msgs::msg::RobotTrajectory trajectory;
-
-                // const double jump_threshold = 0.0;
-                // const double eef_step = 1.0;
-
-                // double fraction;
-
-                // while(fraction < 0.95){
-                //     fraction = move_group_interface.computeCartesianPath(path_to_safe_start, eef_step, jump_threshold, trajectory);
-                // }
-                // double fraction = move_group_interface.computeCartesianPath(path_to_safe_start, eef_step, jump_threshold, trajectory);
-                // moveit::planning_interface::MoveGroupInterface::Plan my_plan2;
-                // my_plan2.trajectory_ = trajectory;
-                // move_group_interface.execute(my_plan2);
-
-                // if (fraction < 0.95) { 
-                //     RCLCPP_ERROR(logger, "Only achieved %.2f%% of the path", fraction * 100);
-                // }
+                RCLCPP_INFO(logger, "Moved to safe start pose");
 
                 control->state_ = SplineFollower::State::MOVE_TO_INTERMEDIATE_POS;
                 break;
@@ -183,6 +55,9 @@ int main(int argc, char * argv[])
 
             case SplineFollower::State::MOVE_TO_INTERMEDIATE_POS:
             {
+
+                RCLCPP_INFO(logger, "State: Move to intermediate pos.");
+
                 // Ensure we have a next spline to move to
                 if (control->current_spline_index_ < control->spline_data_["splines"].size()) {
                     
@@ -204,24 +79,11 @@ int main(int argc, char * argv[])
 
                     move_group_interface.setPoseTarget(control->intermediate_pose_);
                     move_group_interface.move();
-
-                    // moveToPose(intermediate_pose_);
-
-                    // // ✅ Get the current pose of the robot
-                    // geometry_msgs::msg::Pose current_pose = getCurrentRobotPose();
-
-                    // ✅ Compute a straight-line interpolated path to the intermediate poses
-                    // std::vector<geometry_msgs::msg::Pose> path_to_intermediate =
-                    //     computeLinearInterpolationPath(current_pose, intermediate_pose, 100);
-
-                    // // ✅ Execute the trajectory to move to the intermediate pose
-                    // executeTrajectory(path_to_intermediate);
-
-                    // ✅ Move to the next state once the move is complete
                     control->state_ = SplineFollower::State::MOVE_TO_CANVAS;
+                    RCLCPP_INFO(logger, "Moved to intermediate pose.");
                 } 
                 else {
-                    // ✅ If no more splines left, go to STOP state
+                    // If no more splines left, go to STOP state
                     control->state_ = SplineFollower::State::STOP;
                 }
                 break;
@@ -229,86 +91,86 @@ int main(int argc, char * argv[])
 
             case SplineFollower::State::MOVE_TO_CANVAS:
             {
+                RCLCPP_INFO(logger, "State: Move to canvas.");
                 // // ✅ Calculate the average Z position of the canvas
                 double canvas_z = control->calculateAverageCanvasHeight();
 
-                // // // ✅ Get the current pose of the robot
-                // // geometry_msgs::msg::Pose current_pose = getCurrentRobotPose();
-
-                // // ✅ Set the target pose (same x, y as current pose, but at canvas height)
                 control->canvas_pose_ = control->intermediate_pose_;
                 control->canvas_pose_.position.z = canvas_z;  // Move to the canvas surface
 
                 move_group_interface.setPoseTarget(control->canvas_pose_);
                 move_group_interface.move();
 
+                RCLCPP_INFO(logger, "Moved to canvas.");
 
-                // moveToPose(canvas_pose_);
-
-                // // // ✅ Compute a straight-line interpolated path to the canvas
-                // // std::vector<geometry_msgs::msg::Pose> path_to_canvas =
-                // //     computeLinearInterpolationPath(current_pose, canvas_pose, 10);
-
-                // // // ✅ Execute the trajectory to move to the canvas
-                // // executeTrajectory(path_to_canvas);
-
-                // // ✅ Move to next state to start drawing
                 control->state_ = SplineFollower::State::MOVE_THROUGH_DRAWING_TRAJECTORY;
                 break;
             }
 
             case SplineFollower::State::MOVE_THROUGH_DRAWING_TRAJECTORY:
-                // // generateDrawingTrajectory();
-                // if (executeTrajectory(current_trajectory_)) {
-                //     current_spline_index_++;
-                //     state_ = State::MOVE_OFF_CANVAS;
-                // } else {
-                //     RCLCPP_ERROR(this->get_logger(), "Failed to execute drawing trajectory.");
-                //     state_ = State::STOP;
-                // }
-                // std::cout << "Reached here" << std::endl;
-                // generateDrawingTrajectory();
-                // move_group_->execute(drawing_trajectory_);
-                // current_spline_index_++;
-                // state_ = State::MOVE_OFF_CANVAS;
+            {
+                RCLCPP_INFO(logger, "State: Move through drawing trajectory.");
+                std::vector<geometry_msgs::msg::Pose> waypoints;
+                for (const auto& waypoint : control->spline_data_["splines"].at(control->current_spline_index_)["waypoints"]) {
+                    geometry_msgs::msg::Pose pose;
+                    pose.position.x = waypoint[0].get<double>();
+                    pose.position.y = waypoint[1].get<double>();
+                    pose.position.z = waypoint[2].get<double>();  // you can add lift here if needed
+            
+                    pose.orientation.x = 0.0;
+                    pose.orientation.y = 1.0;
+                    pose.orientation.z = 0.0;
+                    pose.orientation.w = 0.0;
+            
+                    waypoints.push_back(pose);
+                }
+            
+                moveit_msgs::msg::RobotTrajectory trajectory;
+                double fraction = move_group_interface.computeCartesianPath(waypoints, 0.01, 0.0, trajectory);
+            
+                std::cout << "Acheieved " << fraction << " of the path " << std::endl;
+
+                RCLCPP_INFO(logger, "Achieved: %ld", control->current_spline_index_*100, " percent of the path.");
+            
+                if (fraction < 0.95) {
+                    RCLCPP_ERROR(logger, "Failed to compute drawing trajectory (%.2f%%).", fraction * 100);
+                }
+                RCLCPP_INFO(logger, "Completed spline: %ld", control->current_spline_index_);
+
+                control->state_ = SplineFollower::State::MOVE_OFF_CANVAS;
+
                 break;
+            }
 
             case SplineFollower::State::MOVE_OFF_CANVAS:
             {
-                // // // ✅ Get the current pose of the robot
-                // // geometry_msgs::msg::Pose current_pose = getCurrentRobotPose();
+                RCLCPP_INFO(logger, "State: Move off canvas.");
+                // Get the canvas height
+                double canvas_z = control->calculateAverageCanvasHeight();
             
-                // // ✅ Get the canvas height
-                // double canvas_z = calculateAverageCanvasHeight();
-            
-                // // ✅ Compute the safe lifted pose (100mm above the canvas)
-                // geometry_msgs::msg::Pose lifted_pose = canvas_pose_;
-                // lifted_pose.position.z = canvas_z + 0.1;  // Move 100mm up
+                // Compute the safe lifted pose (100mm above the canvas)
+                geometry_msgs::msg::Pose lifted_pose = control->canvas_pose_;
+                lifted_pose.position.z = canvas_z + 0.1;  // Move 100mm up
 
-                // moveToPose(canvas_pose_);
+                move_group_interface.setPoseTarget(lifted_pose);
+                move_group_interface.move();
+
             
-                // // // ✅ Compute a straight-line interpolated trajectory
-                // // std::vector<geometry_msgs::msg::Pose> path_off_canvas =
-                // //     computeLinearInterpolationPath(current_pose, lifted_pose, 10);
-            
-                // // ✅ Execute the trajectory to lift off the canvas
-                // // executeTrajectory(path_off_canvas);
-            
-                // // ✅ Move to the next state (going to the next spline or stopping)
-                // current_spline_index_++;
-                // if (current_spline_index_ < spline_data_["splines"].size()) {
-                //     state_ = State::MOVE_TO_INTERMEDIATE_POS;
-                // } else {
-                //     state_ = State::STOP;
-                // }
+                // Move to the next state (going to the next spline or stopping)
+                control->current_spline_index_++;
+                if (control->current_spline_index_ < control->spline_data_["splines"].size()) {
+                    RCLCPP_INFO(logger, "Moved off canvas.");
+                    control->state_ = SplineFollower::State::MOVE_TO_INTERMEDIATE_POS;
+                } else {
+                    control->state_ = SplineFollower::State::STOP;
+                }
             
                 break;
             }                
 
             case SplineFollower::State::STOP:
-                // moveToSafeEndPose();
-                // stopExecution();
-                // RCLCPP_INFO(logger, "All splines completed. Stopping MoveIt and shutting down...");
+                RCLCPP_INFO(logger, "State: Stop.");
+                RCLCPP_INFO(logger, "All splines completed.");
                 return 0;
         }
     }
--- ur3_control/src/spline_follower.hpp
+++ ur3_control/src/spline_follower.hpp
@@ -23,13 +23,9 @@ using json = nlohmann::json;
 class SplineFollower : public rclcpp::Node {
 public:
     SplineFollower();
-    void on_activate();
 
     void setSafeStartPose();
-
-    // void load_parameters_from_yaml(std::shared_ptr<rclcpp::Node> node, const std::string &yaml_file);
-
-    std::vector<geometry_msgs::msg::Pose> computeLinearInterpolationPath(
+    const std::vector<geometry_msgs::msg::Pose> computeLinearInterpolationPath(
             const geometry_msgs::msg::Pose& start_pose, 
             const geometry_msgs::msg::Pose& end_pose, 
             int num_waypoints);
@@ -47,66 +43,23 @@ public:
 
     State state_;
 
-    // State state_;
-    std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group_;
     nlohmann::json spline_data_;
-    // geometry_msgs::msg::Pose safe_start_pose_;
     geometry_msgs::msg::Pose safe_end_pose_;
     std::vector<geometry_msgs::msg::Pose> current_trajectory_;
     const double LIFT_HEIGHT = 0.05;  // 50mm lift height
     size_t current_spline_index_;
 
-    void runStateMachine();
     void addGroundPlane();
     bool loadSplines();
-    geometry_msgs::msg::Pose parsePose(const YAML::Node& node);
-    // void setSafeStartPose();
-    void generateIntermediateTrajectory();
-    void generateDrawingTrajectory();
-    bool executeTrajectory(const std::vector<geometry_msgs::msg::Pose>& waypoints);
-    void executePoseTarget(const geometry_msgs::msg::Pose& target_pose, const std::string& description);
-    void moveToSafeEndPose();
-    void stopExecution();
-
-    // Stores the remaining splines to process
     std::vector<json> remaining_splines_;
 
-    // Stores the intermediate and drawing trajectories
-    moveit_msgs::msg::RobotTrajectory intermediate_trajectory_;
-    moveit_msgs::msg::RobotTrajectory drawing_trajectory_;
-
-    // Helper function for getting waypoint pose
-    geometry_msgs::msg::Pose getWaypointPose(const json& waypoint, double lift);
-
-    // std::vector<geometry_msgs::msg::Pose> computeLinearInterpolationPath(
-    //     const geometry_msgs::msg::Pose& start_pose, 
-    //     const geometry_msgs::msg::Pose& end_pose, 
-    //     int num_waypoints);
-
     double calculateAverageCanvasHeight();
-
-
-    tf2_ros::Buffer tf_buffer_;
-    tf2_ros::TransformListener tf_listener_;
-
-    geometry_msgs::msg::Pose getCurrentRobotPose();
-
-    void moveToPose(geometry_msgs::msg::Pose pose);
 
     geometry_msgs::msg::Pose intermediate_pose_;
 
     geometry_msgs::msg::Pose canvas_pose_;
 
 private:
-    // enum class State {
-    //     INIT,
-    //     MOVE_TO_INTERMEDIATE_POS,
-    //     MOVE_TO_CANVAS,
-    //     MOVE_THROUGH_DRAWING_TRAJECTORY,
-    //     MOVE_OFF_CANVAS,
-    //     STOP
-    // };
-
     
 
 };
