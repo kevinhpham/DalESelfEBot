439777fc5bc7aeecd521dbd16b2a988a39adf084
wilthecodewizard <wilcoxon007@hotmail.com>
2025-04-30 20:51:14 +1000

    Draft readme instructions, requirements.txt to doen load python pacage need to run img_prc

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
+DRAFT BY WIL 
+
 # DalESelfEBot
 ROS2 Framework for a Selfie Drawing Robot Implementation using the UR3/UR3e
+
+## system requirments
+OS: Ubuntu 22.04
+ROS2 Distro: ROS2 Humble Hawksbill
+Python 3.10+
+
+####################################################################################
+
+### SETUP
+
+## Step 1: Create and Build the ROS2 Workspace
+
+# Create workspace directories
+mkdir -p ~/daleselfebot_ws/src
+cd ~/daleselfebot_ws
+
+# Clone the repository into the src folder
+cd src
+git clone https://github.com/JayDeeBot/DalESelfEBot.git
+
+# Go back to workspace root and install dependencies
+cd ~/daleselfebot_ws
+rosdep install --from-paths src --ignore-src -r -y
+
+# Build the workspace
+**use this to exclude unfinshed nodes: (rebuild after any nodes are edited)
+
+colcon build --packages-select img_prc_interface img_prc ur3_localisation
+
+
+**use this when system is complete:
+colcon build
+
+## Step 2: Install Python Dependencies
+
+cd ~/daleselfebot_ws/src/DalESelfEBot
+pip3 install --user -r requirements.txt
+
+
+
+
+
+###################################################################
+
+### Maunal launch Nodes
+
+## Image Processing
+# in new termainal. Source ROS2 & the Workspace. launch image processing
+source /opt/ros/humble/setup.bash
+source ~/daleselfebot_ws/install/setup.bash
+ros2 run img_prc image_processor
+
+
+## GUI
+# in new termainal. Source ROS2 & the Workspace. launch GUI script
+source /opt/ros/humble/setup.bash
+source ~/daleselfebot_ws/install/setup.bash
+python3 ~/daleselfebot_ws/src/DalESelfEBot/GUI/gui_node.py
+
+
+
+
+
--- img_prc/setup.py
+++ img_prc/setup.py
@@ -1,18 +1,15 @@
 from setuptools import find_packages, setup
-from setuptools.command.install import install
-import subprocess
-import time
+# Removed: from setuptools.command.install import install
+# Removed: import subprocess
+# Removed: import time
+import os # Keep os if needed for data_files path construction
 
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        install.run(self)
-        for _ in range(5):  # Retry up to 5 times
-            try:
-                subprocess.check_call(['pip', 'install', 'rembg', 'onnxruntime','opencv-python'])
-                break
-            except subprocess.CalledProcessError:
-                time.sleep(5)  # Wait for 5 seconds before retrying
+# Removed the entire PostInstallCommand class definition
+# class PostInstallCommand(install):
+#     """Post-installation for installation mode."""
+#     def run(self):
+#         install.run(self)
+#         # ... (removed pip install logic) ...
 
 package_name = 'img_prc'
 
@@ -21,28 +18,34 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        # Make sure the path construction is correct for your setup
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Add other data files if needed (e.g., launch files)
+        # (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
     ],
-    setup_requires=['setuptools'],
+    # setup_requires=['setuptools'], # Often not needed with modern pip/setuptools
     install_requires=[
-        'setuptools',
-        'rclpy',
-        'sensor_msgs',
-        'opencv-python',
-        'numpy',
-        'Pillow',
-        'onnxruntime',
+        'setuptools', # Keep setuptools requirement
+        'rclpy',      # ROS 2 Python library
+        'sensor_msgs',# For Image messages
+        # 'opencv-python', # REMOVED - Rely on ROS environment's OpenCV
+        'numpy<2.0',  # Keep numpy, specify compatible version if needed
+        'Pillow',     # Image library used by rembg/cv2 interactions
+        'onnxruntime',# Dependency for rembg
+        'rembg'       # Background removal library (will be installed via pip in venv)
+        # Add other direct Python dependencies of your nodes here
     ],
-    cmdclass={
-        'install': PostInstallCommand,
-    },
+    # Removed cmdclass argument:
+    # cmdclass={
+    #     'install': PostInstallCommand,
+    # },
     zip_safe=True,
-    maintainer='dingus',
-    maintainer_email='dingus@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='dingus', # TODO: Update maintainer info
+    maintainer_email='dingus@todo.todo', # TODO: Update email
+    description='TODO: Package description', # TODO: Update description
+    license='TODO: License declaration', # TODO: Update license
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
